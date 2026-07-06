eec9f093424d381d0484db18e5e356054d9fbb6b
unknown <aku.paajanen1@gmail.com>
2026-07-06 11:09:33 +0300

    Added more separator line styles and the time the calculation takes is now also printed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime};
+
 fn fibonacci(n: i32) -> i32 {
     match n {
         0 => 0,
@@ -6,29 +8,60 @@ fn fibonacci(n: i32) -> i32 {
     }
 }
 
-fn start_calculation() {
+fn print_separator(style: &str) {
+    if style == "1" {
+        println!("===============");
+    } else if style == "2" {
+        println!("---------------");
+    } else if style == "3" {
+        println!("###############");
+    } else if style == "4" {
+        println!("***************");
+    } else if style == "5" {
+        return;
+    } else {
+        println!("===============");
+    }
+}
+
+fn start_calculation(separator_style: &str) {
     println!("Which fibonacci number do you want to print? (0 is the lowest)");
     let mut choice = String::new();
     std::io::stdin().read_line(&mut choice).expect("Failed to read line");
     let count: i32 = choice.trim().parse().unwrap();
-    println!("===============");
+    print_separator(separator_style);
     println!("You selected the number: {}", count);
-    println!("===============");
+    print_separator(separator_style);
+    
+    let time_now = SystemTime::now();
+
     let final_number = fibonacci(count);
     println!("Number {} of the sequence: {}", count, final_number);
-    println!("===============");
+    match time_now.elapsed() {
+        Ok(elapsed) => {
+            println!("Time elapsed: {} ms", elapsed.as_millis());
+        }
+        Err(e) => {
+            println!("Ran into an error: {}", e)
+        }
+    }
+    print_separator(separator_style);
 }
 
 fn main() {
-    start_calculation();
+    println!("Choose separator line style: 1. = (default) \n2. - \n3. # \n4. * \n5. No separators");
+    let mut choice = String::new();
+    std::io::stdin().read_line(&mut choice).expect("Failed to read line");
+    let separator_style: &str = choice.trim();
+    start_calculation(separator_style);
     loop {
         println!("1. Print another number 2. Exit");
         let mut choice = String::new();
         std::io::stdin().read_line(&mut choice).expect("Failed to read line");
         let trimmed_choice = choice.trim();
         if trimmed_choice == "1" {
-            println!("===============");
-            start_calculation()
+            print_separator(separator_style);
+            start_calculation(separator_style)
         } else {
             return;
         }
